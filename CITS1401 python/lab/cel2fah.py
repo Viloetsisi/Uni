9507ba1898028077ac21ced1ed750df8ed577fb4
Viloetsisi <163311946+Viloetsisi@users.noreply.github.com>
2025-01-13 12:01:24 +0800

    Create cel2fah.py

---
+++ CITS1401 python/lab/cel2fah.py
@@ -0,0 +1,9 @@
+# File: cel2fah.py
+# A simple program is illustrating Celsius to Fahrenheit conversion
+def c2f():
+    print("This program converts Celsius into Fahrenheit")
+    cel = float(input("Enter temperature in Celsius: "))
+    fah = 9/5*cel+32
+    print("The temperature in Fahrenheit is:",fah)
+    print("End of the program.")
+
